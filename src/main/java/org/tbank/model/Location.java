b7aa6fb138d5ac606ec4467d8a8b2577a9cf1c68
ddasfasdfs <glibsoldatenko@gmail.com>
2024-09-03 16:23:16 +0300

    feat: add location models

---
+++ src/main/java/org/tbank/model/Location.java
@@ -0,0 +1,13 @@
+package main.java.org.tbank.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+    private String slug;
+    private Coords coords;
+}
