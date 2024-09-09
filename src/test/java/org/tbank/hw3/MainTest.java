83d9258f40b3b3c3687db5445e3f42387184b6e7
ddasfasdfs <glibsoldatenko@gmail.com>
2024-09-09 22:27:56 +0300

    test: add test for custom toCustomLinkedList collector

--- src/test/java/org/tbank/hw3/MainTest.java
+++ src/test/java/org/tbank/hw3/MainTest.java
@@ -8,6 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
+    void fillCustomLinkedListWithStreamApiAndCollectMethod() {
+        CustomLinkedList<Integer> customLinkedList = Main.fillCustomLinkedListWithStreamApiAndCollectMethod();
+
+        assertEquals(5, customLinkedList.size());
+
+        assertEquals(1, customLinkedList.get(0));
+        assertEquals(2, customLinkedList.get(1));
+        assertEquals(3, customLinkedList.get(2));
+        assertEquals(4, customLinkedList.get(3));
+        assertEquals(5, customLinkedList.get(4));
+    }
+
+    @Test
     public void testFillCustomLinkedListWithStreamApi() {
         CustomLinkedList<Integer> customLinkedList = Main.fillCustomLinkedListWithStreamApi();
 
