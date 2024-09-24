7c4651ea2daf147f3e52d5761cfb70d52d837361
ddasfasdfs <glibsoldatenko@gmail.com>
2024-09-24 19:44:19 +0300

    feat: add controller and service for location

---
+++ src/main/java/org/tbank/hw5/controller/LocationController.java
@@ -0,0 +1,27 @@
+package org.tbank.hw5.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.tbank.hw5.dto.LocationDto;
+import org.tbank.hw5.dto.ResponseDto;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/locations")
+public interface LocationController {
+
+    @GetMapping
+    ResponseEntity<ResponseDto<List<LocationDto>>> getAllLocations();
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseDto<LocationDto>> getLocationById(@PathVariable String id);
+
+    @PostMapping
+    ResponseEntity<ResponseDto<LocationDto>> createLocation(@RequestBody LocationDto locationDto);
+
+    @PutMapping("/{id}")
+    ResponseEntity<ResponseDto<LocationDto>> updateLocation(@PathVariable String id, @RequestBody LocationDto locationDto);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteLocation(@PathVariable String id);
+}
---
+++ src/main/java/org/tbank/hw5/service/impl/LocationServiceImpl.java
@@ -0,0 +1,43 @@
+package org.tbank.hw5.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.tbank.hw5.service.LocationService;
+import org.tbank.hw5.storage.impl.LocationStorage;
+import org.tbank.hw5.model.Location;
+
+import java.util.List;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+
+    private final LocationStorage locationStorage;
+
+    public LocationServiceImpl(LocationStorage locationStorage) {
+        this.locationStorage = locationStorage;
+    }
+
+    @Override
+    public List<Location> getAllLocations() {
+        return locationStorage.findAll();
+    }
+
+    @Override
+    public Location getLocationById(String id) {
+        return locationStorage.findById(id);
+    }
+
+    @Override
+    public Location createLocation(Location location) {
+        return locationStorage.save(location.getSlug(), location);
+    }
+
+    @Override
+    public Location updateLocation(String id, Location location) {
+        return locationStorage.update(id, location);
+    }
+
+    @Override
+    public void deleteLocation(String id) {
+        locationStorage.deleteById(id);
+    }
+}
