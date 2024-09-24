74d528adc3c18aa2009e4f80bcbf114f7e1b1069
ddasfasdfs <glibsoldatenko@gmail.com>
2024-09-24 16:44:18 +0300

    feat: add CustomErrorResponse

---
+++ src/main/java/org/tbank/hw5/exceptions/model/CustomErrorResponse.java
@@ -0,0 +1,10 @@
+package org.tbank.hw5.exceptions.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CustomErrorResponse {
+    private String message;
+}
