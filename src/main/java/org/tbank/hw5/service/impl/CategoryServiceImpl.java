e411d43fcc9279addeb0862e3383987d6a63497f
ddasfasdfs <glibsoldatenko@gmail.com>
2024-09-24 16:44:07 +0300

    feat: create CategoryServiceImpl

---
+++ src/main/java/org/tbank/hw5/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,44 @@
+package org.tbank.hw5.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.tbank.hw5.annotation.LogExecutionTime;
+import org.tbank.hw5.model.Category;
+import org.tbank.hw5.service.CategoryService;
+import org.tbank.hw5.storage.impl.CategoryStorage;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryStorage categoryStorage;
+
+    public CategoryServiceImpl(CategoryStorage categoryStorage) {
+        this.categoryStorage = categoryStorage;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryStorage.findAll();
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryStorage.findById(id);
+    }
+
+    @Override
+    public Category createCategory(Category category) {
+        return categoryStorage.save(category.getId(), category);
+    }
+
+    @Override
+    public Category updateCategory(Long id, Category category) {
+        return categoryStorage.update(id, category);
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryStorage.deleteById(id);
+    }
+}
