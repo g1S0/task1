165c74d8d5c95c9073c6132b6b2740598ca0c6a6
ddasfasdfs <glibsoldatenko@gmail.com>
2024-09-30 18:48:56 +0300

    feat: add exception handler for api request

--- src/main/java/org/tbank/hw5/client/ApiClient.java
+++ src/main/java/org/tbank/hw5/client/ApiClient.java
@@ -4,6 +4,7 @@ import org.springframework.web.client.RestClient;
 import org.springframework.stereotype.Component;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.web.client.RestClientException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,18 +22,25 @@ public abstract class ApiClient<T> {
     public List<T> fetchFromApi(String apiUrl, Class<T[]> responseType) {
         logger.info("Fetching {} from API at {}", responseType.getSimpleName(), apiUrl);
 
-        T[] responseArray = restClient
-                .get()
-                .uri(apiUrl)
-                .retrieve()
-                .body(responseType);
+        try {
+            T[] responseArray = restClient
+                    .get()
+                    .uri(apiUrl)
+                    .retrieve()
+                    .body(responseType);
 
-        if (responseArray == null || responseArray.length == 0) {
-            logger.error("Failed to fetch {} from API at {}.", responseType.getSimpleName(), apiUrl);
-            throw new IllegalStateException("No " + responseType.getSimpleName() + " fetched from API.");
+            if (responseArray == null || responseArray.length == 0) {
+                logger.error("Failed to fetch {} from API at {}.", responseType.getSimpleName(), apiUrl);
+                throw new IllegalStateException("No " + responseType.getSimpleName() + " fetched from API.");
+            }
+
+            logger.info("Successfully fetched {} from API.", responseType.getSimpleName());
+            return Arrays.asList(responseArray);
+
+        } catch (Exception e) {
+            logger.error("Error occurred while fetching {} from API at {}. Message: {}",
+                    responseType.getSimpleName(), apiUrl, e.getMessage());
+            throw new IllegalStateException("Failed to fetch " + responseType.getSimpleName() + " from API at " + apiUrl, e);
         }
-
-        logger.info("Successfully fetched {} from API.", responseType.getSimpleName());
-        return Arrays.asList(responseArray);
     }
 }
