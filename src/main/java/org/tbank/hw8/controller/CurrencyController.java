5d77715dfc35a2c1545c98e38e40dfb9bbe9e81e
ddasfasdfs <glibsoldatenko@gmail.com>
2024-10-10 00:53:04 +0300

    feat: add swagger

--- src/main/java/org/tbank/hw8/controller/CurrencyController.java
+++ src/main/java/org/tbank/hw8/controller/CurrencyController.java
@@ -1,8 +1,14 @@
 package org.tbank.hw8.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.tbank.hw8.exception.model.CustomErrorResponse;
 import org.tbank.hw8.dto.ConvertedAmountDto;
 import org.tbank.hw8.dto.CurrencyRateDto;
 import org.tbank.hw8.dto.CurrencyConversionRequestDto;
@@ -10,9 +16,39 @@ import org.tbank.hw8.dto.CurrencyConversionRequestDto;
 @RequestMapping("/currencies")
 public interface CurrencyController {
 
+    @Operation(summary = "Get currency rate by code")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CurrencyRateDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class))),
+            @ApiResponse(responseCode = "503", description = "Service Unavailable",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class)))
+    })
     @GetMapping("/rates/{code}")
     ResponseEntity<CurrencyRateDto> getCurrencyRate(@Valid @PathVariable String code);
 
+    @Operation(summary = "Convert currency")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ConvertedAmountDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class))),
+            @ApiResponse(responseCode = "503", description = "Service Unavailable",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomErrorResponse.class)))
+    })
     @PostMapping("/convert")
     ResponseEntity<ConvertedAmountDto> convertCurrency(@Valid @RequestBody CurrencyConversionRequestDto request);
 }
