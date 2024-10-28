92db4c16eb46ad0f63eca848a8221dcac26dd007
ddasfasdfs <glibsoldatenko@gmail.com>
2024-10-28 17:40:54 +0300

    chore: remove extra annotation

--- src/main/java/org/tbank/hw10/exception/EntityNotFoundException.java
+++ src/main/java/org/tbank/hw10/exception/EntityNotFoundException.java
@@ -1,9 +1,7 @@
 package org.tbank.hw10.exception;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
 @EqualsAndHashCode(callSuper = true)
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
--- src/main/java/org/tbank/hw10/exception/RelatedEntityNotFoundException.java
+++ src/main/java/org/tbank/hw10/exception/RelatedEntityNotFoundException.java
@@ -1,9 +1,7 @@
 package org.tbank.hw10.exception;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
 @EqualsAndHashCode(callSuper = true)
 public class RelatedEntityNotFoundException extends RuntimeException {
     public RelatedEntityNotFoundException(String message) {
