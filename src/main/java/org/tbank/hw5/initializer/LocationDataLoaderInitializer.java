d1ec474bea8e09336dc209f2a0ffe3ecefca9874
ddasfasdfs <glibsoldatenko@gmail.com>
2024-10-29 13:34:54 +0300

    feat: add Observer pattern for data saving

--- src/main/java/org/tbank/hw5/initializer/LocationDataLoaderInitializer.java
+++ src/main/java/org/tbank/hw5/initializer/LocationDataLoaderInitializer.java
@@ -1,33 +1,28 @@
 package org.tbank.hw5.initializer;
 
+import lombok.AllArgsConstructor;
 import org.example.annotation.LogExecutionTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.tbank.hw5.client.LocationApiClient;
 import org.tbank.hw5.dto.LocationDto;
 import org.tbank.hw5.mapper.LocationMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.tbank.hw5.model.Location;
-import org.tbank.hw5.storage.impl.LocationStorage;
+import org.tbank.hw5.storage.DataObserver;
 
 import java.util.List;
 
 @Component
+@AllArgsConstructor
 public class LocationDataLoaderInitializer {
     private static final Logger logger = LoggerFactory.getLogger(LocationDataLoaderInitializer.class);
 
     private final LocationApiClient locationApiClient;
-    private final LocationStorage locationStorage;
+    private final List<DataObserver<Location>> dataObservers;
     private final LocationMapper locationMapper;
-
-    public LocationDataLoaderInitializer(LocationApiClient locationApiClient, LocationStorage locationStorage, LocationMapper locationMapper) {
-        this.locationApiClient = locationApiClient;
-        this.locationStorage = locationStorage;
-        this.locationMapper = locationMapper;
-    }
-
 
     @EventListener(ApplicationReadyEvent.class)
     @LogExecutionTime
@@ -39,14 +34,18 @@ public class LocationDataLoaderInitializer {
         List<Location> locations = locationMapper.toLocationList(locationsDto);
 
         if (locations != null) {
-            for (Location location : locations) {
-                locationStorage.save(location.getSlug(), location);
-            }
+            notifyObservers(dataObservers, locations);
             logger.info("Location data source successfully initialized with {} locations.", locations.size());
         } else {
             logger.warn("No locations found to initialize data source.");
         }
 
         logger.info("Location data source initialization completed.");
+    }
+
+    private <T> void notifyObservers(List<DataObserver<T>> observers, List<T> data) {
+        for (DataObserver<T> observer : observers) {
+            observer.update(data);
+        }
     }
 }
--- src/main/java/org/tbank/hw5/storage/impl/CategoryStorage.java
+++ src/main/java/org/tbank/hw5/storage/impl/CategoryStorage.java
@@ -2,8 +2,18 @@ package org.tbank.hw5.storage.impl;
 
 import org.springframework.stereotype.Repository;
 import org.tbank.hw5.model.Category;
+import org.tbank.hw5.model.Location;
+import org.tbank.hw5.storage.DataObserver;
 import org.tbank.hw5.storage.LocalStorage;
 
+import java.util.List;
+
 @Repository
-public class CategoryStorage extends LocalStorage<Long, Category> {
+public class CategoryStorage extends LocalStorage<Long, Category> implements DataObserver<Category> {
+    @Override
+    public void update(List<Category> elements) {
+        for (Category category : elements) {
+            save(category.getId(), category);
+        }
+    }
 }
