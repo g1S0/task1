456a4b55eeec66b52ad30f071257cba830059d2e
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-07 13:16:07 +0300

    test: fix previous tests for hm8

--- src/test/java/org/tbank/hw8/controller/impl/CurrencyControllerConvertIT.java
+++ src/test/java/org/tbank/hw8/controller/impl/CurrencyControllerConvertIT.java
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 public class CurrencyControllerConvertIT {
 
     @Autowired
