e84d035a0edb5e36db424cf16353e701aab30826
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-11 14:09:20 +0300

    chore: change variable init

--- src/main/java/org/tbank/hw8/configuration/JwtAuthenticationFilter.java
+++ src/main/java/org/tbank/hw8/configuration/JwtAuthenticationFilter.java
@@ -41,14 +41,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
         final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userEmail;
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
-        jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);
+        final String jwt = authHeader.substring(7);
+        final String userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
             var isTokenValid = tokenRepository.findByToken(jwt)
