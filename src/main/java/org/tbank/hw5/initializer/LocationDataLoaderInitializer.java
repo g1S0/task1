3522ece70aa8d4eefc95035f17c4ed136d151775
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-11 23:16:01 +0300

    fix: fix conflicts

--- src/main/java/org/tbank/hw5/initializer/LocationDataLoaderInitializer.java
+++ src/main/java/org/tbank/hw5/initializer/LocationDataLoaderInitializer.java
@@ -7,41 +7,41 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.tbank.hw5.client.CategoryApiClient;
-import org.tbank.hw5.dto.CategoryDto;
-import org.tbank.hw5.mapper.CategoryMapper;
-import org.tbank.hw5.model.Category;
+import org.tbank.hw5.client.LocationApiClient;
+import org.tbank.hw5.dto.LocationDto;
+import org.tbank.hw5.mapper.LocationMapper;
+import org.tbank.hw5.model.Location;
 import org.tbank.hw5.storage.DataObserver;
 
 import java.util.List;
 
 @Component
 @AllArgsConstructor
-public class CategoryDataLoaderInitializer implements Command {
-    private static final Logger logger = LoggerFactory.getLogger(CategoryDataLoaderInitializer.class);
+public class LocationDataLoaderInitializer implements Command {
+    private static final Logger logger = LoggerFactory.getLogger(LocationDataLoaderInitializer.class);
 
-    private final CategoryApiClient categoryApiClient;
-    private final List<DataObserver<Category>> dataObservers;
-    private final CategoryMapper categoryMapper;
+    private final LocationApiClient locationApiClient;
+    private final List<DataObserver<Location>> dataObservers;
+    private final LocationMapper locationMapper;
 
     @Override
     @EventListener(ApplicationReadyEvent.class)
     @LogExecutionTime
     public void execute() {
-        logger.info("Starting data source for categories");
+        logger.info("Starting location data source for locations");
 
-        List<CategoryDto> categoriesDto = categoryApiClient.fetchCategories();
+        List<LocationDto> locationsDto = locationApiClient.fetchLocations();
 
-        List<Category> categories = categoryMapper.toCategoryList(categoriesDto);
+        List<Location> locations = locationMapper.toLocationList(locationsDto);
 
-        if (categories != null) {
-            notifyObservers(dataObservers, categories);
-            logger.info("Data source successfully initialized with {} categories.", categories.size());
+        if (locations != null) {
+            notifyObservers(dataObservers, locations);
+            logger.info("Location data source successfully initialized with {} locations.", locations.size());
         } else {
             logger.warn("No locations found to initialize data source.");
         }
 
-        logger.info("Data source initialization completed.");
+        logger.info("Location data source initialization completed.");
     }
 
     private <T> void notifyObservers(List<DataObserver<T>> observers, List<T> data) {
