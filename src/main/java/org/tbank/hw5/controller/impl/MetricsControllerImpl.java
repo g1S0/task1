9423e2a367c7c6cade3e3be986657d366f1e8cdd
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-27 23:57:55 +0300

    feat: add out-of-memory and stack-overflow methods

--- src/main/java/org/tbank/hw5/controller/impl/MetricsControllerImpl.java
+++ src/main/java/org/tbank/hw5/controller/impl/MetricsControllerImpl.java
@@ -4,6 +4,9 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MetricsControllerImpl {
 
@@ -17,5 +20,24 @@ public class MetricsControllerImpl {
     public String incrementCounter() {
         meterRegistry.counter("custom_metric", "type", "requests").increment();
         return "Request received!";
+    }
+
+    @GetMapping("/stack-overflow")
+    public String triggerStackOverflow() {
+        recursiveMethod();
+        return "This will never be reached";
+    }
+
+    private void recursiveMethod() {
+        recursiveMethod();
+    }
+
+    @GetMapping("/out-of-memory")
+    public String triggerOutOfMemory() {
+        List<int[]> list = new ArrayList<>();
+        while (true) {
+            int[] array = new int[1000000];
+            list.add(array);
+        }
     }
 }
